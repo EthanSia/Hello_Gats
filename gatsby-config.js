933bad30aa52288feb8d84173d37e36c1bb4454b
Ethan Alexandro Sia <d00225319@student.dkit.ie>
2022-02-02 14:13:41 +0000

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
-    siteMetadata: {
-      title: "Hello",
-    },
-    plugins: [
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
-    ],
-  };
+  siteMetadata: {
+    title: "Hello",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
+  ],
+};
