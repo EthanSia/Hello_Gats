2e1c39b8adb6464647e0d42929f180ade79657d9
Ethan Alexandro Sia <d00225319@student.dkit.ie>
2022-05-05 14:19:52 +0100

    my commit

---
+++ Hello_Gats-main/src/pages/index.js
@@ -0,0 +1,66 @@
+
+//import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Link from 'gatsby-link'
+import React, {useState} from 'react';
+
+
+
+const IndexPage = () => {
+
+  const [file, setFile] = useState({});
+
+  const changeHandler = (event) => {
+	
+    const {name, value} = event.target
+    if (name === 'file' ){
+      return setFile(value)
+    }
+	};
+
+  const encode = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach((k)=>{
+      formData.append(k,data[k])
+    });
+    return formData
+  }
+
+  const handleSubmission = (event) => {
+		event.preventDefault();
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({ "form-name": "contact", ..." "}),
+  })
+    .then(() => navigate("/thank-you/"))
+    .catch((error) => alert(error));
+	};
+
+  const navigation = useNavigation();
+
+
+  return (
+    <div>
+      <p>I'm making the upload file or link.</p>
+     
+      <form
+       name="contact"
+       method="post"
+       data-netlify="true"
+       data-netlify-honeypot="bot-field"
+       onSubmit={handleSubmission}
+      >
+        <div>
+          <input name="file" placeholder='Link' type="file" onChange={changeHandler}></input>
+          <button onClick={()=> navigation.navigate({handleSubmission})}>Upload</button>
+        </div>
+      
+        
+      </form>
+    </div>
+    
+  )
+}
+
+export default IndexPage
